07b16dd0d664657e15d904ecef67dc276ed319a8
Richardson Bruno <brunor@lonewolfcorp.com>
2020-04-14 00:06:38 -0300

    Conexao ao banco de dados e criacao de usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Welcome to course' });
+routes.get('/teste', async (req, res) => {
+  const user = await User.create({
+    name: 'Richardson Bruno',
+    email: 'richardsonbrc@gmail.com',
+    password_hash: '321321321',
+  });
+  return res.json(user);
 });
 
 export default routes;
